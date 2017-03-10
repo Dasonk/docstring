e2b73d97530cf424fd7f8a3bf80399c33b34a696
Dason Kurkiewicz <dasonk@gmail.com>
2017-03-10 09:54:14 -0500

    First commit

---
+++ R/docstring.R
@@ -0,0 +1,79 @@
+
+#' @import utils
+docstring_to_roxygen <- function(fun, funname = as.character(substitute(fun))){
+    
+    values <- capture.output(print(fun))
+    roxy_ids <- grepl("^[[:space:]]*#\'", values)
+    roxy_strings <- values[roxy_ids]
+    roxy_strings <- gsub("^[[:space:]]*", "", roxy_strings)
+    
+    roxy <- paste0(roxy_strings, collapse = "\n")
+    
+    
+    funargs <- capture.output(args(fun))
+    funargs[1] <- paste(funname, "<-", funargs[1])
+    
+    roxytext <- paste(c(roxy, funargs), collapse = "\n")
+    return(roxytext)
+}
+
+
+#' Display a docstring
+#' 
+#' Display a docstring using R's built in help file viewer.
+#' 
+#' @param fun The function that has the docstring you would like to display
+#' @param default_title The title you would like to display if no title is detected
+#' in the docstring itself
+#' @param warnings logical Whether you want warning messages displayed in the console
+#' @export
+#' @import roxygen2
+#' @import utils
+docstring <- function(fun, default_title = "Title not detected", warnings = TRUE){
+    
+    funname <- as.character(substitute(fun))
+    
+    # Extract the roxygen style comments from the function's code
+    roxytext <- docstring_to_roxygen(fun, funname = funname)
+    
+    # The general approach is to create a shell of a package
+    # and create an R file in the R directory in which we write
+    # out the roxygen comments followed by a minimal function
+    # definition. We roxygenize the fake package and then
+    # view the resulting help file. Afterwards we remove
+    # the files and folders we create
+    
+    
+    tdir <- tempdir()
+    packagedir <- file.path(tdir, "TempPackage")
+    if(file.exists(packagedir)){
+        unlink(packagedir, recursive = TRUE)
+    }
+    on.exit(unlink(packagedir, recursive = TRUE))
+    
+    # We need 
+    
+    j <- new.env(parent = emptyenv())
+    j$a <- 0
+    package.skeleton(name = "TempPackage", path = tdir, environment = j)
+    
+    if(!file.exists(file.path(packagedir, "R"))){
+        dir.create(file.path(packagedir, "R"))
+    } 
+    
+    tfile <- file.path(packagedir, "R", paste0(funname, ".R"))
+    # tfile <- tempfile(pattern = funname, 
+    #                   fileext = ".R", 
+    #                   tmpdir = paste0(packagedir, "/R"))
+    cat(roxytext, file = tfile)
+    
+    roxygenize(packagedir, "rd")
+    
+    file <- file.path(packagedir, "man", paste0(funname, ".Rd"))
+    
+    temp <- tools::Rd2HTML(file, tempfile(fileext = ".html"))
+    
+    browseURL(temp)
+    
+    return(invisible())
+}
---
+++ README.md
@@ -0,0 +1,67 @@
+# docstring
+
+The docstring package is an R package that provides the ability to 
+display something analagous to
+Python's docstrings within R.  By allowing the user to document
+their functions as comments at the beginning of their function
+without requiring putting the function into a package we allow
+more users to easily provide documentation for their functions.
+The documentation can be viewed using an accessor function but
+displays just like any other R help files.
+
+The user will need to be familiar with roxygen style comments (via the roxygen2 package)
+to fully utilize the package.  Eventually I want to detect basic comments and
+allow the user to just use those as the documentation without requiring
+the use of the roxygen keywords at least for simple cases.  Ideally this will
+allow users not yet comfortable with package creation to still provide
+documentation for their functions. If they use the roxygen style comments when it
+is time to convert their work into a package all they will need to do is move
+their pre-existing documentation outside of the function and they will be set.
+
+## Examples
+
+```r
+library(docstring)
+
+square <- function(x){
+
+    #' Square a number
+    #'
+    #' Calculates the square of the input
+    #'
+    #' @param x the input to be squared
+
+    return(x^2)
+}
+
+docstring(square)
+```
+
+![Square](inst/square.png)
+
+
+
+## Installation
+
+The package is not yet on CRAN but is available to download using devtools.
+
+You can also download the dev version via [zip ball](https://github.com/dasonk/docstring/zipball/master) or [tar ball](https://github.com/dasonk/docstring/tarball/master), decompress and run `R CMD INSTALL` on it, or use the **devtools** package to install the development version:
+
+```r
+## Make sure your current packages are up to date
+update.packages()
+## devtools is required
+library(devtools)
+install_github("dasonk/docstring")
+```
+
+Note: Windows users need [Rtools](http://www.murdoch-sutherland.com/Rtools/) and [devtools](http://CRAN.R-project.org/package=devtools) to install this way.
+
+
+## Contact
+
+You are welcome to:
+* submit suggestions and bug-reports at: <https://github.com/dasonk/docstring/issues>
+* send a pull request on: <https://github.com/dasonk/docstring/>
+
+
