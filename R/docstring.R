919e7ef20c2d96e9189442853ffee2ffbdffc2e7
Dason Kurkiewicz <dasonk@gmail.com>
2017-03-24 09:57:11 -0400

    Added examples to docstring

--- R/docstring.R
+++ R/docstring.R
@@ -87,7 +87,61 @@ has_docstring <- function(fun, fun_name = as.character(substitute(fun))){
 #' @importFrom utils package.skeleton
 #' @importFrom utils browseURL
 #' @aliases ?
-#'
+#' 
+#' @examples 
+#' \dontrun{
+#' square <- function(x){
+#'   #' Square a number
+#'   #'
+#'   #' Calculates the square of the input
+#'   #' 
+#'   #' @param x the input to be squared
+#' 
+#'   return(x^2)
+#' }
+#' 
+#' docstring(square)
+#' ?square
+#' 
+#' 
+#' mypaste <- function(x, y = "!"){
+#'   #' Paste two items
+#'   #' 
+#'   #' @description This function pastes two items
+#'   #' together.  
+#'   #'
+#'   #' By using the description tag you'll notice that I
+#'   #' can have multiple paragraphs in the description section
+#'   #' 
+#'   #' @param x character. The first item to paste
+#'   #' @param y character. The second item to paste Defaults to "!" but
+#'   #' "?" would be pretty great too
+#'   #' @usage mypaste(x, y)
+#'   #' @return The inputs pasted together as a character string.
+#'   #' @details The inputs can be anything that can be input into
+#'   #' the paste function.
+#'   #' @note And here is a note. Isn't it nice?
+#'   #' @section I Must Warn You:
+#'   #' The reference provided is a good read.
+#'   #' \subsection{Other warning}{
+#'   #'   It is completely irrelevant to this function though.
+#'   #' }
+#'   #' 
+#'   #' @references Tufte, E. R. (2001). The visual display of 
+#'   #' quantitative information. Cheshire, Conn: Graphics Press.
+#'   #' @examples
+#'   #' mypaste(1, 3)
+#'   #' mypaste("hey", "you")
+#'   #' mypaste("single param")
+#'   #' @export
+#'   #' @importFrom base paste
+#'   
+#'   return(paste(x, y))
+#' }
+#'   
+#' ?mypaste
+#' }
+#' 
 #' @export
 docstring <- function(fun, fun_name = as.character(substitute(fun)),
                       rstudio_pane = getOption("docstring_rstudio_help_pane"),
